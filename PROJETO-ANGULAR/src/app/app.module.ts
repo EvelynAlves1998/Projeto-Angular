81189252166221693372230f2ccc42eb149285f2
Evelyn Alves da Silva <evelyn.silva@nava.com.br>
2022-03-30 14:58:31 -0300

    adicionado componentes manual e gerado pela cli

--- PROJETO-ANGULAR/src/app/app.module.ts
+++ PROJETO-ANGULAR/src/app/app.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CliComponent } from './cli/cli.component';
 import { ComponentCliComponent } from './component-cli/component-cli.component';
-
+import { ComponentManualComponent } from './component-manual/component-manual.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CliComponent,
-    ComponentCliComponent
+    ComponentCliComponent,
+    ComponentManualComponent
   ],
   imports: [
     BrowserModule
---
+++ PROJETO-ANGULAR/src/app/component-manual/component-manual.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-manual',
+  templateUrl: './component-manual.component.html',
+  styleUrls: ['./component-manual.component.css']
+})
+export class ComponentManualComponent{
+
+  constructor() { }
+  
+}
